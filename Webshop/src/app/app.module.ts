b7cd88725adf101e97891a89cc7d3f0e679000d4
tobias van der vaart <113106074+TobiasVdvaart@users.noreply.github.com>
2024-04-03 11:35:00 +0200

    errors

--- Webshop/src/app/app.module.ts
+++ Webshop/src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'; 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component';
+import { GamesComponent } from './components/games.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent }, 
+    { path: 'games', component: GamesComponent } 
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        GamesComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes) 
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
